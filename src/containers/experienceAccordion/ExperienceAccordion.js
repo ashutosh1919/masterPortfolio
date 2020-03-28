bab7a9557bc5af21d42303cdcb34b2d1f1c6ba0b
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-03-28 11:29:11 +0530

    Added Accordion consisting Panels of Work, Internships and Volunteerships.

--- src/containers/experienceAccordion/ExperienceAccordion.js
+++ src/containers/experienceAccordion/ExperienceAccordion.js
@@ -1,32 +1,26 @@
-import React, { Component }from "react";
-import './ExperienceAccordion.css';
-import {Accordion, Panel} from 'baseui/accordion';
-import {Slide} from 'react-reveal';
+import React, { Component } from "react";
+import "./ExperienceAccordion.css";
+import { Accordion, Panel } from "baseui/accordion";
+import { Slide } from "react-reveal";
 
 class ExperienceAccordion extends Component {
-    render(){
-        return (
-					<div className="experience-accord">
-						<Accordion onChange={({ expanded }) => console.log(expanded)}>
-							<Slide right>
-								<Panel className="accord-panel" title="Work">
-									Content 1
-								</Panel>
-							</Slide>
-							<Slide right>
-								<Panel className="accord-panel" title="Internships">
-									Content 2
-								</Panel>
-							</Slide>
-							<Slide right>
-								<Panel className="accord-panel" title="Volunteerships">
-									Content 3
-								</Panel>
-							</Slide>
-						</Accordion>
-					</div>
-				);
-    }
+	render() {
+		return (
+			<div className="experience-accord">
+				<Accordion onChange={({ expanded }) => console.log(expanded)}>
+					<Panel className="accord-panel" title="Work">
+						Content 1
+					</Panel>
+					<Panel className="accord-panel" title="Internships">
+						Content 2
+					</Panel>
+					<Panel className="accord-panel" title="Volunteerships">
+						Content 3
+					</Panel>
+				</Accordion>
+			</div>
+		);
+	}
 }
 
 export default ExperienceAccordion;
