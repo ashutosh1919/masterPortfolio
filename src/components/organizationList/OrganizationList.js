1fe5c90769469d7fa012df5c03cab08a350c4dc4
Larbi Sahli <larbisahli1905@gmail.com>
2020-07-18 18:49:22 +0100

    adding transition and styling tooltip

--- src/components/organizationList/OrganizationList.js
+++ src/components/organizationList/OrganizationList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './OrganizationList.css';
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
-import {Fade} from "react-reveal";
+import { OverlayTrigger } from "react-bootstrap";
+import { Fade } from "react-reveal";
 
 class OrganizationList extends Component {
     render() {
@@ -15,17 +15,17 @@ class OrganizationList extends Component {
                                 placement={"top"}
                                 style={{ marginBottom: "5px" }}
                                 overlay={
-                                    <Tooltip id={`tooltip-top`}>
+                                    <div className="tooltip-container" id={`tooltip-top`}>
                                         <strong>{logo["login"]}</strong>
-                                    </Tooltip>
+                                    </div>
                                 }
                             >
                                 <li className="organizations-inline" name={logo["login"]}>
                                     <Fade bottom duration={2000} distance="40px">
-                                    <img className="organizations-img" src={logo["avatarUrl"]} alt={logo["login"]} />
+                                        <img className="organizations-img" src={logo["avatarUrl"]} alt={logo["login"]} />
                                     </Fade>
                                 </li>
-                            
+
                             </OverlayTrigger>
                         );
                     })}
