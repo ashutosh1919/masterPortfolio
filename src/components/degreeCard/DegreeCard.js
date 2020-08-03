a33bf92e55ead1cdbad42bfc3154b947d5b4b066
Jatin Chauhan <40722235+mrjatinchauhan@users.noreply.github.com>
2020-08-03 22:18:01 +0530

    display original logo size
    It looks more better and visual.

--- src/components/degreeCard/DegreeCard.js
+++ src/components/degreeCard/DegreeCard.js
@@ -14,7 +14,6 @@ class DegreeCard extends Component {
               style={{
                 maxWidth: "100%",
                 maxHeight: "100%",
-                transform: "scale(50%, 50%)",
               }}
               src={require(`../../assests/images/${degree.logo_path}`)}
               alt={degree.alt_name}
