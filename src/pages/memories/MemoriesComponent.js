eedf680dc4deccb65bdd4cc0e30fa671aacc2253
Tanya Goel <41191504+Tanya-Goel@users.noreply.github.com>
2020-08-12 19:08:11 +0530

    Create MemoriesComponent.js
    Added Memories Component

---
+++ src/pages/memories/MemoriesComponent.js
@@ -0,0 +1,60 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { memories } from "../../portfolio";
+import React, { Component } from "react";
+import { Fade } from "react-reveal";
+import "./MemoriesComponent.css";
+
+const MemoriesData = memories.memoriesSection;
+
+class Memories extends Component {
+  render() {
+    const theme = this.props.theme;
+    return (
+      <div>
+        <Header theme={theme} />
+        <Fade bottom duration={2000} distance="20px">
+          <div className="heading-div">
+            <h1 className="headingtext" style={{ color: theme.text }}>
+              Some Precious Moments 😇{" "}
+            </h1>
+          </div>
+          <div className="imgg-div">
+            <img src={require("../../assests/images/img1.jpg")} alt="" />
+
+            <img src={require("../../assests/images/m.jpg")} alt="" />
+          </div>
+          <div className="imgg-div">
+            <img src={require("../../assests/images/img3.jpg")} alt="" />
+
+            <img src={require("../../assests/images/img4.jpg")} alt="" />
+          </div>
+
+          <div className="heading-img-div">
+            <img src={require("../../assests/images/img5.jpeg")} alt="" />
+          </div>
+          <div className="heading-img-div">
+            <img src={require("../../assests/images/img6.jpg")} alt="" />
+          </div>
+          <div className="heading-img-div">
+            <img src={require("../../assests/images/img7.jpg")} alt="" />
+          </div>
+          <div className="imgg-div">
+            <img src={require("../../assests/images/img8.jpg")} alt="" />
+            <img src={require("../../assests/images/img9.jpg")} alt="" />
+          </div>
+          <div className="imgg-div">
+            <img src={require("../../assests/images/img13.JPG")} alt="" />
+            <img src={require("../../assests/images/img11.jpeg")} alt="" />
+          </div>
+          <div className="heading-img-div">
+            <img src={require("../../assests/images/img12.jpg")} alt="" />
+          </div>
+        </Fade>
+        <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
+      </div>
+    );
+  }
+}
+
+export default Memories;
