1949b58f4a3dbc92661aa178cc1ee5f2dc7cbb33
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-08-13 13:51:16 +0530

    Update src/pages/memories/MemoriesComponent.js

--- src/pages/memories/MemoriesComponent.js
+++ src/pages/memories/MemoriesComponent.js
@@ -1,6 +1,5 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
-import { memories } from "../../portfolio";
 import React, { Component } from "react";
 import { Fade } from "react-reveal";
 import "./MemoriesComponent.css";
