a334482c96abc0dd99b1b234cf315965d227ef36
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-08-14 11:49:40 +0530

    Added YouTube in Social Media

--- README.md
+++ README.md
@@ -159,14 +159,13 @@ Therefore, this part of portfolio is not customizable. But don't worry we have a
 - The last step in deploying is to enable `Github Pages` in settings of the repository and select `gh-pages` branch.
 
 Now, your website is successfully deployed and you can visit it at `<your-github-username>.github.io`.  
-If you are stuck somewhere and want to observe the deployment process in depth, then please watch below video.  
-  
+If you are stuck somewhere and want to observe the deployment process in depth, then please watch below video.
+
 <p align="center"> 
     <a href="http://www.youtube.com/watch?v=IwBS39TOmpA" target="_blank">
     <img src="http://img.youtube.com/vi/IwBS39TOmpA/0.jpg"></img>
   </a>
 </p>
-
 
 # Technologies used 🛠️
 
@@ -212,6 +211,7 @@ If you can help us with these. Please don't hesitate to open an [pull request](h
 
 <!-- markdownlint-enable -->
 <!-- prettier-ignore-end -->
+
 <!-- ALL-CONTRIBUTORS-LIST:END -->
 
 # References 👏🏻
--- src/portfolio.js
+++ src/portfolio.js
@@ -18,51 +18,57 @@ const greeting = {
 };
 
 const socialMediaLinks = [
-	/* Your Social Media Link */
-	// github: "https://github.com/ashutosh1919",
-	// linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-	// gmail: "ashutoshhathidara98@gmail.com",
-	// gitlab: "https://gitlab.com/ashutoshhathidara98",
-	// facebook: "https://www.facebook.com/laymanbrother.19/",
-	// twitter: "https://twitter.com/ashutosh_1919",
-	// instagram: "https://www.instagram.com/layman_brother/"
-  
-	{
-		name: "Github",
-		link: "https://github.com/ashutosh1919",
-		fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-		backgroundColor: '#181717' // Reference https://simpleicons.org/?q=github
-	},
-	{
-		name: "LinkedIn",
-		link: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-		fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-		backgroundColor: '#0077B5' // Reference https://simpleicons.org/?q=linkedin
-	},
-	{
-		name: "Gmail",
-		link: "mailto:ashutoshhathidara98@gmail.com",
-		fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-		backgroundColor: '#D14836' // Reference https://simpleicons.org/?q=gmail
-	},
-	{
-		name: "Twitter",
-		link: "https://twitter.com/ashutosh_1919",
-		fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
-		backgroundColor: '#1DA1F2' // Reference https://simpleicons.org/?q=twitter
-	},
-	{
-		name: "Facebook",
-		link: "https://www.facebook.com/laymanbrother.19/",
-		fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
-		backgroundColor: '#1877F2' // Reference https://simpleicons.org/?q=facebook
-	},
-	{
-		name: "Instagram",
-		link: "https://www.instagram.com/layman_brother/",
-		fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-		backgroundColor: '#E4405F' // Reference https://simpleicons.org/?q=instagram
-	},
+  /* Your Social Media Link */
+  // github: "https://github.com/ashutosh1919",
+  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // gmail: "ashutoshhathidara98@gmail.com",
+  // gitlab: "https://gitlab.com/ashutoshhathidara98",
+  // facebook: "https://www.facebook.com/laymanbrother.19/",
+  // twitter: "https://twitter.com/ashutosh_1919",
+  // instagram: "https://www.instagram.com/layman_brother/"
+
+  {
+    name: "Github",
+    link: "https://github.com/ashutosh1919",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  {
+    name: "YouTube",
+    link: "https://www.youtube.com/channel/UC_amoXmmxSY9KusoDczDTXQ",
+    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  },
+  {
+    name: "Gmail",
+    link: "mailto:ashutoshhathidara98@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/ashutosh_1919",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/laymanbrother.19/",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/layman_brother/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
 ];
 
 const skills = {
