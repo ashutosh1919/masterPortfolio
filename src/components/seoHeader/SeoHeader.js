8de6b476b6298a7dd434e57a6e2451db5e175a4f
githubprabin143 <prabinkarki643@gmail.com>
2021-01-31 10:59:57 +0545

    Added SEO Related settings

---
+++ src/components/seoHeader/SeoHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { seo } from "../../portfolio.js";
+
+function SeoHeader() {
+  return (
+    <Helmet>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta property="og:title" content={seo?.og?.title} />
+      <meta property="og:type" content={seo?.og?.type} />
+      <meta property="og:url" content={seo?.og?.url} />
+    </Helmet>
+  );
+}
+
+export default SeoHeader;
