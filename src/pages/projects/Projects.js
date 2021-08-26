96bfbd5ea80c8671a77976cd4ab262246d52b693
priyanshk20 <khandelwalpriyansh85@gmail.com>
2021-08-27 02:08:16 +0530

    added publications section

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
+import PublicationCard from "../../components/publicationsCard/PublicationCard";
 import Button from "../../components/button/Button";
 import TopButton from "../../components/topButton/TopButton";
 import { Fade } from "react-reveal";
-import { projectsHeader } from "../../portfolio.js";
+import { projectsHeader, publicationsHeader } from "../../portfolio.js";
 import ProjectsData from "../../shared/opensource/projects.json";
+import PublicationData from "../../shared/opensource/publications.json";
 import "./Projects.css";
 import ProjectsImg from "./ProjectsImg";
 
@@ -55,6 +57,36 @@ class Projects extends Component {
           newTab={true}
           theme={theme}
         />
+
+        {/* Publications  */}
+
+        <div className="basic-projects">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="publications-heading-div">
+              <div className="publications-heading-text-div">
+                <h1
+                  className="publications-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {publicationsHeader.title}
+                </h1>
+                <p
+                  className="projects-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {publicationsHeader["description"]}
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+
+        <div className="repo-cards-div-main">
+          {PublicationData.data.map((repo) => {
+            return <PublicationCard repo={repo} theme={theme} />;
+          })}
+        </div>
+
         <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
         <TopButton theme={this.props.theme} />
       </div>
