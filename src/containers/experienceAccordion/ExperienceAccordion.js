20b33d904da4492741c953355c8dcfedc733595b
varan5 <varan2.bhalla@gmail.com>
2021-08-30 18:34:41 +0530

    Fixed issue #121: Experience card expanded on initial loading

--- src/containers/experienceAccordion/ExperienceAccordion.js
+++ src/containers/experienceAccordion/ExperienceAccordion.js
@@ -4,17 +4,47 @@ import "./ExperienceAccordion.css";
 import { Accordion, Panel } from "baseui/accordion";
 
 class ExperienceAccordion extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Work: true,
+      Interships: true,
+      Volunteerships: true
+    }
+ }
+
   render() {
     const theme = this.props.theme;
     return (
       <div className="experience-accord">
-        <Accordion onChange={({ expanded }) => console.log(expanded)}>
-          {this.props.sections.map((section) => {
+        <Accordion onPanelChange={(key, onChange) => {console.log('Key: ' + key)}} >
+          {this.props.sections.map((section, index) => {
             return (
               <Panel
                 className="accord-panel"
                 title={section["title"]}
-                key={section["title"]}
+                key={index}
+                value={section["title"]}
+                expanded={
+                  index == 0 ? this.state.Work : (index == 1 ? this.state.Internships : false) 
+                }
+              
+                onClick={(event) => {
+                  console.log('Event', event)
+          
+                  switch(index) {
+                    case 0: {
+                      this.setState({Work: this.state.Word})
+                    }
+                    case 1: {
+                      this.setState({Interships: !this.state.Interships})
+                    }
+                    case 2: {
+                      this.setState({Volunteerships: !this.state.Volunteerships})
+                    }
+                  }
+                }}
               >
                 {section["experiences"].map((experience) => {
                   return (
