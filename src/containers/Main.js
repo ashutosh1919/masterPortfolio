842563638a540aa950075ede71666e190ddce44b
Abdessalam Bahafid <59281502+abdslam01@users.noreply.github.com>
2021-09-16 15:16:24 +0100

    Add 404 page (#149)
    * Add 404 page
    
    * fix UI for 404 page
    
    * revert the version of dependencies

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,6 +8,7 @@ import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
+import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
   render() {
@@ -64,6 +65,12 @@ export default class Main extends Component {
                 path="/projects"
                 render={(props) => (
                   <Projects {...props} theme={this.props.theme} />
+                )}
+              />
+              <Route
+                path="*"
+                render={(props) => (
+                  <Error404 {...props} theme={this.props.theme} />
                 )}
               />
             </Switch>
