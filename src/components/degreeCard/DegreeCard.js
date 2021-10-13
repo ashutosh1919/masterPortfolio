858b2770e57fa6b1b04025cc38354caa047f2fa3
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2021-10-13 20:07:10 +0530

    Fixed upside down school logo

--- src/components/degreeCard/DegreeCard.js
+++ src/components/degreeCard/DegreeCard.js
@@ -14,7 +14,7 @@ class DegreeCard extends Component {
               style={{
                 maxWidth: "100%",
                 maxHeight: "100%",
-                transform: "scale(50%, 50%)",
+                transform: "scale(0.9)",
               }}
               src={require(`../../assests/images/${degree.logo_path}`)}
               alt={degree.alt_name}
