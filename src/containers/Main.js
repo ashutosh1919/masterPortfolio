78483be6dd3cde20995112d799c1eb659389e90d
Dhruv Krishna Vaid <dhruvkrishnavaid@gmail.com>
2022-06-22 17:21:10 +0530

    Miscellaneous changes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
-import Home from "../pages/home/HomeComponent";
+import { settings } from "../portfolio.js";
 import Splash from "../pages/splash/Splash";
+import Home from "../pages/home/HomeComponent";
 import Education from "../pages/education/EducationComponent";
 import Experience from "../pages/experience/Experience";
 import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
-import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
