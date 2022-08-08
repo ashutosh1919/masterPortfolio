92f47e55387a8d2843047ef9c319cede8b3c0bad
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2022-08-08 16:11:45 -0500

    fixed error in minScore

--- lighthouserc.js
+++ lighthouserc.js
@@ -12,6 +12,15 @@ module.exports = {
         "http://localhost:3000/#/contact",
       ],
     },
+    asserts: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.1 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.8 }],
+        "categories:seo": ["error", { minScore: 0.8 }],
+        "categories.pwa": "off",
+      },
+    },
     upload: {
       target: "temporary-public-storage",
     },
