bcb9dde54456edd23e2c20fb59ece8d999b8f316
Juan Betancourt Osorio <juan.betancourto@perficient.com>
2022-08-09 10:59:44 -0500

    aggregattion methid

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,7 +15,10 @@ module.exports = {
     assert: {
       assertions: {
         "categories:performance": ["error", { minScore: 0.1 }],
-        "categories:accessibility": ["error", { minScore: 0.8 }],
+        "categories:accessibility": [
+          "error",
+          { minScore: 0.8, aggregationMethod: "pessimistic" },
+        ],
         "categories:best-practices": ["error", { minScore: 0.8 }],
         "categories:seo": ["error", { minScore: 0.8 }],
         "categories.pwa": "off",
