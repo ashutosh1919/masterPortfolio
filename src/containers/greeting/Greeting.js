c56918a458f070a61ed3a29f457bf6c56512b769
Sai Teja <40917760+saiteja13427@users.noreply.github.com>
2023-04-12 00:21:31 +0530

    Nickname fix (#284)
    * Nickname fix
    
    * Minor Fix

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -17,9 +17,11 @@ export default function Greeting(props) {
               <h1 className="greeting-text" style={{ color: theme.text }}>
                 {greeting.title}
               </h1>
-              <h2 className="greeting-nickname" style={{ color: theme.text }}>
-                ( {greeting.nickname} )
-              </h2>
+              {greeting.nickname && (
+                <h2 className="greeting-nickname" style={{ color: theme.text }}>
+                  ( {greeting.nickname} )
+                </h2>
+              )}
               <p
                 className="greeting-text-p subTitle"
                 style={{ color: theme.secondaryText }}
