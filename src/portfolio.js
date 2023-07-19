a7ba50fb68c580d513172b445282e45c4d4cbe1f
dar123-art <84905414+dar123-art@users.noreply.github.com>
2023-07-19 20:03:01 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const seo = {
 //Home Page
 const greeting = {
   title: "Darshit Kaklotar",
-  logo_name: "DarshitKaklotar",
+  logo_name: "DarshitKaklotar ",
   nickname: "D_K",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
