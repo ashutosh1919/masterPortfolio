db0796f6e1e807f740bd7e0eb483d3ddf5f04007
Syamil <madlim9809@gmail.com>
2023-08-15 23:24:43 +0800

    Add font in splash screen

--- src/portfolio.js
+++ src/portfolio.js
@@ -394,7 +394,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://blogs.syamilthoughts.com/",
+    link: "https://blog.syamilthoughts.com/",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
