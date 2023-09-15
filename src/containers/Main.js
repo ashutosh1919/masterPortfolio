bf3017cae0b53cd6825d5bdc8eef54765b86e0de
vjspranav <pranavasri@live.in>
2023-09-15 15:56:52 +0530

    Improve isSplash conditional rendering
    Signed-off-by: vjspranav <pranavasri@live.in>

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -12,127 +12,65 @@ import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
   render() {
-    if (settings.isSplash) {
-      return (
-        <div>
-          <BrowserRouter basename="/">
-            <Switch>
-              <Route
-                path="/"
-                exact
-                render={(props) => (
-                  <Splash {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/home"
-                render={(props) => <Home {...props} theme={this.props.theme} />}
-              />
-              <Route
-                path="/experience"
-                exact
-                render={(props) => (
-                  <Experience {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/education"
-                render={(props) => (
-                  <Education {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/opensource"
-                render={(props) => (
-                  <Opensource {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/contact"
-                render={(props) => (
-                  <Contact {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/splash"
-                render={(props) => (
-                  <Splash {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/projects"
-                render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="*"
-                render={(props) => (
-                  <Error404 {...props} theme={this.props.theme} />
-                )}
-              />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <BrowserRouter basename="/">
-            <Switch>
-              <Route
-                path="/"
-                exact
-                render={(props) => <Home {...props} theme={this.props.theme} />}
-              />
-              <Route
-                path="/home"
-                render={(props) => <Home {...props} theme={this.props.theme} />}
-              />
-              <Route
-                path="/experience"
-                exact
-                render={(props) => (
-                  <Experience {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/education"
-                render={(props) => (
-                  <Education {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/opensource"
-                render={(props) => (
-                  <Opensource {...props} theme={this.props.theme} />
-                )}
-              />
-              <Route
-                path="/contact"
-                render={(props) => (
-                  <Contact {...props} theme={this.props.theme} />
-                )}
-              />
-              {/* <Route
-							path="/splash"
-							render={(props) => (
-								<Splash
-									{...props}
-									theme={this.props.theme}
-								/>
-							)}
-						/> */}
-              <Route
-                path="/projects"
-                render={(props) => (
-                  <Projects {...props} theme={this.props.theme} />
-                )}
-              />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      );
-    }
+    return (
+      <BrowserRouter basename="/">
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) =>
+              settings.isSplash ? (
+                <Splash {...props} theme={this.props.theme} />
+              ) : (
+                <Home {...props} theme={this.props.theme} />
+              )
+            }
+          />
+          <Route
+            path="/home"
+            render={(props) => <Home {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="/experience"
+            exact
+            render={(props) => (
+              <Experience {...props} theme={this.props.theme} />
+            )}
+          />
+          <Route
+            path="/education"
+            render={(props) => (
+              <Education {...props} theme={this.props.theme} />
+            )}
+          />
+          <Route
+            path="/opensource"
+            render={(props) => (
+              <Opensource {...props} theme={this.props.theme} />
+            )}
+          />
+          <Route
+            path="/contact"
+            render={(props) => <Contact {...props} theme={this.props.theme} />}
+          />
+
+          {settings.isSplash && (
+            <Route
+              path="/splash"
+              render={(props) => <Splash {...props} theme={this.props.theme} />}
+            />
+          )}
+
+          <Route
+            path="/projects"
+            render={(props) => <Projects {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="*"
+            render={(props) => <Error404 {...props} theme={this.props.theme} />}
+          />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
