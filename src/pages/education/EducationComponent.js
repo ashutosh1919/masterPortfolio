77bdbabb95498b80d1097d12f6fe7fc5862d35d5
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-04-26 11:41:24 +0300

    fixed a typo

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -32,7 +32,7 @@ class Education extends Component {
                   Education
                 </h1>
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                  Basic Qualification and Certifcations
+                  Basic Qualification and Certifications
                 </h3>
                 <CompetitiveSites logos={competitiveSites.competitiveSites} />
               </div>
