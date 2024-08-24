c94e9a9b64bb173f9af2055dbd068cc799874a6b
Peter Njuguna <njugunapm23@gmail.com>
2024-08-24 15:15:00 +0300

    made changes to all the css files in the components folder to adopt responsive units for improved flexibility and accessibility

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./assests/font-awesome/css/all.css";
+import "./assets/font-awesome/css/all.css";
 
 const engine = new Styletron();
 
